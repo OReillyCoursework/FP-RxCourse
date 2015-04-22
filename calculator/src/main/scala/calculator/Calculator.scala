d5a5415babb3ca82afdcd08153cd35f207bb1f2e
Ross O'Reilly <oreillyross@gmail.com>
2015-04-22 12:37:18 +0200

    Update Calculator.scala

--- calculator/src/main/scala/calculator/Calculator.scala
+++ calculator/src/main/scala/calculator/Calculator.scala
@@ -11,11 +11,14 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
+        
+        for ( 
           
-          for {
-              cell <- namedExpressions._1  
-              answer <- eval(namedExpressions._2, namedExpressions) 
-          } yield (cell, Var(answer) )
+          (cell,expr) <- namedExpressions; 
+          result = eval(expr) 
+        
+        ) yield (cell,Var(result))
+          
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
