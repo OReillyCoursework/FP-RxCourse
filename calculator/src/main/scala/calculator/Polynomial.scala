8a4a9d61f7b25e63bc175cd37af767ba6405139c
Ross O'Reilly <oreillyross@gmail.com>
2015-04-22 17:12:16 +0200

    Update Polynomial.scala

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -4,14 +4,19 @@ object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
       
-      Var {(b() * b()) - (4 * a() * c()) }      
+      Var {
+        (b() * b()) - (4 * a() * c()) 
+      }      
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+      
+      var set = Set[Double]
+      set += (-b() - sqrt(delta())) / (2*a())
+      set += (-b() + sqrt(delta())) / (2*a())
       Var {
-        //todo   
-        Set(0,1,2)  
+        set      
       }
   }
 }
