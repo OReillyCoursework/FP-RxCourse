50bc76fa687be5427fdb740ecb2f4c9d5bd77f11
Ross O'Reilly <oreillyross@gmail.com>
2015-04-22 10:55:01 +0200

    Update Calculator.scala

--- calculator/src/main/scala/calculator/Calculator.scala
+++ calculator/src/main/scala/calculator/Calculator.scala
@@ -11,12 +11,27 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+          
+          for {
+              cell <- namedExpressions._1  
+              answer <- eval(namedExpressions._2, namedExpressions) 
+          } yield (cell, Var(answer) )
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
-  }
+      
+      expr match {
+          
+            case Literal(v: Double) => v
+            case Ref(name: String) => getReferenceExpr(name, references)
+            case Plus(a: Expr, b: Expr) => eval(a, references) + eval(b, references)
+            case Minus(a: Expr, b: Expr) => eval(a, references) - eval(b, references)
+            case Times(a: Expr, b: Expr) => eval(a, references) * eval(b, references)
+            case Divide(a: Expr, b: Expr) => eval(a, references) / eval(b, references)
+            case _ => Double.NaN
+      }
+  }  
+  
 
   /** Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
