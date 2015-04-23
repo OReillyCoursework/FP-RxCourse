a683c79be2a6545e41a7cc6ce48557d3fb577cf6
Ross O'Reilly <oreillyross@gmail.com>
2015-04-23 14:29:21 +0200

    Update Polynomial.scala

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,32 +1,55 @@
 package calculator
 
-object Polynomial {
-  def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-      
-      Var {
+object Polynomial { 
+  def computeDelta(a: Signal[Double], b: Signal[Double], c: Signal[Double]): Signal[Double] = {
+
         b() * b() - 4.0 * a() * c() 
-      }      
+
   }
 
-  def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-      
+ def computeSolutions(a: Signal[Double], b: Signal[Double], c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+
       var set: Set[Double] = Set()
+
       val aa = a() + a()
-      
+
       if (delta() < 0) {
-        
+
           val re = -b() / aa
+
           val im = math.sqrt(-delta())/aa
+
           // not doing anything with the result of complex roots
-      } else {
-        val re = if (b() < 0) (-b + math.sqrt(delta()))/aa else (-b - math.sqrt(delta()))/aa)
-        // need to add roots to set
+
+      } else if (delta() > 0) {
+
+        
+        var root1 =  (-b() + math.sqrt(delta()))/aa  
+        var root2 =  (-b() - math.sqrt(delta()))/aa  
+        if (root1 != root2) {
+            set += root1
+            set += root2
+        } else 
+            set += root1
+      
+
+      } else {  // delta is zero
+        
+        var root1 =  (-b() + math.sqrt(delta()))/aa  
+        var root2 =  (-b() - math.sqrt(delta()))/aa  
+        if (root1 != root2) {
+            set += root1
+            set += root2
+        } else 
+            set += root1
+
       }
 
-      Var {
         set      
-      }
+
   }
+
 }
+
+
+
